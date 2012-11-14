77b1cc9e2f5a4ae7b32cc43bfd57ae8f30febd91
Andreas Bolin <andbo467@student.liu.se>
2012-11-14 18:04:20 +0100

    -

--- trunc/carpark/bilpark.cc
+++ trunc/carpark/bilpark.cc
@@ -1,10 +1,10 @@
 /*
 * FILENAMN:    bilpark.cc
 * PROJECT:     A3B
-* PROGRAMMER:  Conny Andersson  910124-2411 Y3a conan414@student.liu.se
-*              Andreas Bolin    820406-2098 Y3a andbo467@student.liu.se
-*              Martin Andersson 890929-0566 Y3a maran703@student.liu.se
-*              Adam Andersson               Y3a adaan690@student.liu.se
+* PROGRAMMER:  Conny Andersson  Y3a conan414@student.liu.se
+*              Andreas Bolin    Y3a andbo467@student.liu.se
+*              Martin Andersson Y3a maran703@student.liu.se
+*              Adam Andersson   Y3a adaan690@student.liu.se
 * DATE:        2012-10-12
 *
 * DESCRIPTION
