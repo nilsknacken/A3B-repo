fe8b2f6314ea8e1bc89faa206dce9c5057a2a0d6
Andreas Bolin <andbo467@student.liu.se>
2012-11-15 13:29:53 +0100

    andbo467 sandbox

---
+++ sandbox-andbo467/Carpark.cc
@@ -0,0 +1,27 @@
+/*
+* FILENAME:    Carpark.cc
+* PROJECT:     A3B
+* PROGRAMMER:  Conny Andersson  Y3a conan414@student.liu.se
+*              Andreas Bolin    Y3a andbo467@student.liu.se
+*              Martin Andersson Y3a maran703@student.liu.se
+*              Adam Andersson   Y3a adaan690@student.liu.se
+* DATE:        2012-11-15
+*
+* DESCRIPTION
+* 
+* ///////////////Programbeskrivning
+*/
+
+#include <string>
+#include "Carpark.h"
+#include "Database.h"
+#include "Gui.h"
+
+
+Carpark(string filename)
+{
+   Database db(filename);
+   Gui gui;
+   
+   
+}
---
+++ sandbox-andbo467/Carpark.h
@@ -0,0 +1,18 @@
+#ifndef CARPARK_H
+#define CARPARK_H
+
+#include <string>
+
+class Carpark
+{
+public:
+   Carpark(std::string);
+   ~Carpark(){};
+   
+   
+   
+private:
+   
+}
+
+#endif
