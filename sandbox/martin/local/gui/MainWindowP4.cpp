1172d3b7e89d89fe63f0a8de714cbb5433916a41
Martin Andersson <Martin@c-213-196.eduroam.liu.se>
2012-11-30 14:37:46 +0100

    MWP4 på g

--- sandbox/martin/local/gui/MainWindowP4.cpp
+++ sandbox/martin/local/gui/MainWindowP4.cpp
@@ -1,21 +1,45 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 #include <QDialog>
+#include <vector>
 
+#include <iostream>
 
+#include "../backend/ReservationQ.h"
+#include "../backend/Search_reservationQ.h"
 
+using namespace std;
 
-////// Tab 4 - Checkin
+////// Tab 4 - Återlämna
 void MainWindow::on_pushButtonP4next_clicked()
 {
     int index = ui->stackedWidgetP4->currentIndex();
 
-    // Första sidan
+    // Första sidan -> sida2
     if(index == 0)
     {
-        if (ui->tableWidget_2->)
-        ui->stackedWidgetP4->setCurrentIndex(++index);
-        ui->pushButtonP4back->setDisabled(false);
+
+  /*      int currentRow = ui->tableWidget_2->currentRow();
+        if (currentRow >= 0)
+        {
+            ui->labelP4reg_nr_var->setText(search_resP4.get_current_result()[currentRow]->get_reg_nr());
+            ui->labelP4checkout_var->setText(search_resP4.get_current_result()[currentRow]->get_start());
+            ui->labelP4checkin_var->setText(search_resP4.get_current_result()[currentRow]->get_end());
+            ui->lineEditP4mileage->setText(search_resP4.get_current_result()[currentRow]->get_mileage());
+            ui->plainTextEditP4damages->setPlainText(search_resP4.get_current_result()[currentRow]->get_damages());
+*/
+            ui->stackedWidgetP4->setCurrentIndex(++index);
+            ui->pushButtonP4back->setDisabled(false);
+  /*      }
+        else
+        {
+            QMessageBox::information(this,
+                                     QString::fromUtf8("Välj post"),
+                                     QString::fromUtf8("Vänligen välj en post innan du klickar på nästa."),
+                                     QMessageBox::Ok);
+        }
+*/
+
     }
 
     else if(index == 1)
@@ -44,11 +68,14 @@ void MainWindow::on_pushButtonP4back_clicked()
     {
         ui->stackedWidgetP4->setCurrentIndex(--index);
         ui->pushButtonP4back->setDisabled(true);
+        ui->pushButtonP4next->setText(QString::fromUtf8("Nästa >"));
     }
 
     if(index == 2)
     {
         ui->stackedWidgetP4->setCurrentIndex(--index);
+        ui->pushButtonP4next->setText(QString::fromUtf8("Nästa >"));
+
     }
 }
 
@@ -58,7 +85,7 @@ void MainWindow::on_checkBoxP4damages_yes_toggled(bool checked)
 }
 
 
-// Initiate tableWidet
+// Initiate tableWidget
 void MainWindow::setup_tableWidgetP4() const
 {
     ui->tableWidget_2->setColumnCount(6);
